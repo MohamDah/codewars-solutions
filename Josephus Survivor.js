c99855b497548ef08cf00aa4603107748d58b243
MohamDah <mohammedfaiseldhb@gmail.com>
2025-01-20 12:14:24 +0200

    commit for Josephus Survivor.js

---
+++ Josephus Survivor.js
@@ -0,0 +1,14 @@
+function josephusSurvivor(n,k){
+  const arr = new Array(n).fill(0).map((i, ind) => ind+1);
+  let next = (k - 1) % arr.length;
+  
+  while (true) {
+    if (arr.length === 1) return arr[0];
+    arr.splice(next, 1);
+    next = (next + k - 1) % arr.length;
+  }
+}
+
+console.log(
+  josephusSurvivor(11, 19)
+)
