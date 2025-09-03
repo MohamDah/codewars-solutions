e2bdb7f70ef1f08f6a3847c79841bb72e6d1c8cd
MohamDah <mohammedfaiseldhb@gmail.com>
2025-09-03 09:47:40 +0200

    commit for Pig Sursurunga.js

---
+++ Pig Sursurunga.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/5536aba6e4609cc6a600003d/javascript
+function sursurungal(txt) {
+  return txt.replace(/([0-9]+) ([a-z]+)/gi, (match, num, word) => {
+    num = +num;
+    if (num < 2) return match;
+    word = word.replace(/s$/, "");
+    return num === 2 ? `${num} bu${word}`
+      : num <= 9 ? `${num} ${word}zo`
+        : `${num} ga${word}ga`;
+  });
+}
+
+console.log(sursurungal(
+  // "2 tomatoes"
+  `3 tails of rat
+2 legs of snake
+1 eye of owl
+631 ears of bat`
+));
+
+`
+3 tailzo of rat
+2 buleg of snake
+1 eye of owl
+631 gaearga of bat`;
