b321b6657ce7a4d6e0e6ef0c5c8627e039f49df6
MohamDah <mohammedfaiseldhb@gmail.com>
2025-09-05 09:39:55 +0200

    commit for Number Format.js

---
+++ Number Format.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/565c4e1303a0a006d7000127
+var numberFormat = function (number) {
+  return number.toLocaleString()
+};
+
+console.log(numberFormat(
+  // 100000
+  // 5678545
+  -420902
+))
