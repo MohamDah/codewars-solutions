a5614c027076374b44b89c90d7d821580cbd362b
MohamDah <mohammedfaiseldhb@gmail.com>
2025-09-08 09:01:26 +0200

    commit for Is a number prime?.js

---
+++ Is a number prime?.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5262119038c0985a5b00029f
+function isPrime(num) {
+  if (num < 2) return false;
+  const sqrt = Math.sqrt(num);
+  for (let i = 2; i <= sqrt; i++)
+    if (num % i === 0) return false;
+  return true;
+}
